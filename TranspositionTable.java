201ab31743c761ce697b36d428782d29543f181e
Brayden T <mailforbrade@gmail.com>
2024-12-14 04:01:52 -0500

    Optimize transposition table and fix bug with key-value pairs storage
    - Fixed existing bug with the transposition table where it wasn't storing the key-value pairs correctly
    - Optimized the transposition table by using two arrays to store the key-value pairs instead of an object
    - Optimized the index() method to use bitwise operations instead of modulo
    - Resulted in a 17% improvement in overall computation time (41ms/pos -> 34ms/pos for middle-easy positions)
    - Updated the toString() method to return the key and value array instead of the deprecated table array

--- TranspositionTable.java
+++ TranspositionTable.java
@@ -2,43 +2,73 @@ import java.util.Arrays;
 
 public class TranspositionTable {
     
-    private static class Entry {
+    private class Entry {
         long key;
         byte value;
+
+        public Entry() {
+            key = 0;
+            value = 0;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%d %d", key, value);
+        }
+        
     }
 
-    private Entry[] table;
+    //private Entry[] table;
+    private long[] keys;
+    private byte[] values;
+    private int size;
 
     public TranspositionTable(int size) {
-        this.table = new Entry[size];
+        //this.table = new Entry[size];
+        this.keys = new long[size];
+        this.values = new byte[size];
+        this.size = size;
         reset();
     }
 
     public void reset() {
-        Arrays.fill(table, new Entry());
+        //for (int i = 0; i < table.length; i++) {
+            //table[i] = new Entry();
+        //}
+        for (int i = 0; i < size; i++) {
+            keys[0] = 0;
+            values [0] = 0;
+        }
     }
 
     public void put(long key, byte value) {
         int index = index(key);
-        table[index].key = key;
-        table[index].value = value;
+        keys[index] = key;
+        values[index] = value;
+        //table[index].key = key;
+        //table[index].value = value;
     }
 
     public byte get(long key) {
         int index = index(key);
-        if (table[index].key == key) {
-            return table[index].value;
+        if (keys[index] == key) {
+            return values[index];
         }
+
+        //if (table[index].key == key) {
+            //return table[index].value;
+        //}
         return 0;
     }
 
     private int index(long key) {
-        return (int) (key % table.length);
+        //return (int) (key % table.length);
+        return (int) (key & (size - 1));
     }
 
     @Override
     public String toString() {
-        return Arrays.toString(table);
+        return Arrays.toString(keys) + "\n" + Arrays.toString(values);
     }
 
 
