2a826bb5bd93422cf35987264a629f0b325f643b
Brayden T <mailforbrade@gmail.com>
2024-12-15 19:23:52 -0500

    Create a new java file to play Connect 4
    - Created a new java file to allow two players to play Connect 4 until one player wins or until they both draw

---
+++ Play.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Play {
+    public static void main(String[] args){
+
+        BitBoard board = new BitBoard();
+        Solver solver = new Solver(board);
+        int player = 1;
+        Scanner input = new Scanner(System.in);
+
+        while (!board.checkWinner(1) && !board.checkWinner(0) && !board.checkDraw()) {
+            System.out.println(board);
+            System.out.println("Enter move: ");
+            int move = input.nextInt();
+            input.nextLine();
+            
+            if (board.isColumnFull(move - 1)) {
+                System.out.printf("Columns %d is full\n", move);
+                continue;
+            }
+            board.placeDisc(move - 1, player);
+
+            if (board.checkWinner(1)) {
+                break;
+            }
+
+            player = solver.getOpponent(player);
+
+            if (board.checkWinner(0)) {
+                break;
+            }
+        }
+
+        System.out.println(board);
+
+        if (board.checkWinner(1)) {
+            System.out.println("Player 1 wins");
+        } else if (board.checkWinner(0)) {
+            System.out.println("Player 2 wins");
+        } else {
+            System.out.println("Draw");
+        }
+
+    }
+}
