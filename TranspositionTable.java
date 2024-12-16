f3523d9a02a4f0a6212a32c09d0bbc11da68bf61
Brayden T <mailforbrade@gmail.com>
2024-12-15 19:18:28 -0500

    Refactor TranspositionTable to remove unused code
    - Removed the deprecated inner Entry class
    - Removed commented-out code

--- TranspositionTable.java
+++ TranspositionTable.java
@@ -1,30 +1,12 @@
 import java.util.Arrays;
 
 public class TranspositionTable {
-    
-    private class Entry {
-        long key;
-        byte value;
 
-        public Entry() {
-            key = 0;
-            value = 0;
-        }
-
-        @Override
-        public String toString() {
-            return String.format("%d %d", key, value);
-        }
-        
-    }
-
-    //private Entry[] table;
     private long[] keys;
     private byte[] values;
     private int size;
 
     public TranspositionTable(int size) {
-        //this.table = new Entry[size];
         this.keys = new long[size];
         this.values = new byte[size];
         this.size = size;
@@ -32,9 +14,6 @@ public class TranspositionTable {
     }
 
     public void reset() {
-        //for (int i = 0; i < table.length; i++) {
-            //table[i] = new Entry();
-        //}
         for (int i = 0; i < size; i++) {
             keys[0] = 0;
             values [0] = 0;
@@ -45,8 +24,6 @@ public class TranspositionTable {
         int index = index(key);
         keys[index] = key;
         values[index] = value;
-        //table[index].key = key;
-        //table[index].value = value;
     }
 
     public byte get(long key) {
@@ -54,15 +31,10 @@ public class TranspositionTable {
         if (keys[index] == key) {
             return values[index];
         }
-
-        //if (table[index].key == key) {
-            //return table[index].value;
-        //}
         return 0;
     }
 
     private int index(long key) {
-        //return (int) (key % table.length);
         return (int) (key & (size - 1));
     }
 
@@ -70,6 +42,4 @@ public class TranspositionTable {
     public String toString() {
         return Arrays.toString(keys) + "\n" + Arrays.toString(values);
     }
-
-
 }
