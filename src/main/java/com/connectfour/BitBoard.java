aa0e33d42a63c6cad145353e570d0c6048552b5c
Brayden T <mailforbrade@gmail.com>
2024-12-15 22:14:34 -0500

    Change package name to be correct

--- src/main/java/com/connectfour/BitBoard.java
+++ src/main/java/com/connectfour/BitBoard.java
@@ -1,4 +1,4 @@
-package src.main.java.com.connectfour;
+package com.connectfour;
 
 public class BitBoard {
     
--- src/main/java/com/connectfour/TranspositionTable.java
+++ src/main/java/com/connectfour/TranspositionTable.java
@@ -1,4 +1,4 @@
-package src.main.java.com.connectfour;
+package com.connectfour;
 
 import java.util.Arrays;
 
