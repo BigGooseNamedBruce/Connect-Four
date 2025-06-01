809c6f88f7b36f9ea86ae1b24447a671d23aae2f
Brayden T <mailforbrade@gmail.com>
2025-05-31 21:09:23 -0400

    Refactor SolverTest.java to improve clarity and reduce duplication

--- backend/src/test/java/com/connectfour/SolverTest.java
+++ backend/src/test/java/com/connectfour/SolverTest.java
@@ -1,203 +1,79 @@
 package com.connectfour;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.time.Instant;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.List;
+import java.nio.file.Path;
 
 import com.connectfour.game.*;
 
 public class SolverTest {
 
-    BitBoard board;
-    Solver solver;
-    static int count = 0;
+    BitBoard board = new BitBoard();;
+    Solver solver =  new Solver(board);
     char player;
 
-
-    @BeforeEach
-    void setUp() {
+    @ParameterizedTest
+    @MethodSource("testFilesProvider")
+    void testMethodWithFileData(String filepath) throws Exception {
+        List<String> data = loadDataFromFile(Path.of(filepath));
         board = new BitBoard();
         solver = new Solver(board);
-        count = 0;
         player = 'r';
-    }
 
-    @Test
-    void endEasyTest() {
+        for (String line: data) {
+            String[] parts = line.split(" ");
+            String position = parts[0];
+            int score = Integer.parseInt(parts[1]);
+            
+            player = board.load(position);
+            System.out.println(line);
+            assertEquals(score, solver.solve(board, player));
 
-        System.out.println("Testing Test_L3_R1_End_Easy.txt");
+            board.clear();
+            player = 'r';
 
-        File file = new File("src/test/resources/Test_L3_R1_End_Easy.txt");
-
-        try {
-            Scanner scanner = new Scanner(file);
-
-            while (scanner.hasNextLine()) {
-                count++;
-                String line = scanner.nextLine();
-                String[] parts = line.split(" ");
-                String moveOrder = parts[0];
-                int score = Integer.parseInt(parts[1]);
-
-                for (int i = 0; i < moveOrder.length(); i++) {
-                    board.placeDisc(Character.getNumericValue(moveOrder.charAt(i) - 1), player);
-                    player = solver.getOpponent(player);
-                }
-
-                int testScore = solver.solve(player);
-
-                if (testScore == score) {
-                    System.out.println("True " + count);
-                } else {
-                    System.out.println("False: " + testScore + " != " + score + " " + count);
-                }
-                
-                assertEquals(score, testScore, "dasdad");
-
-                board.clear();
-
-            }
-        } catch (FileNotFoundException fileNotFoundException) {
-            System.out.printf("Test_L3_R1_End_Easy.txt not found");
         }
     }
 
-    @Test
-    void MiddleEasyTest() {
-
-        System.out.println("Testing Test_L2_R1_Middle_Easy.txt");
-
-        File file = new File("src/test/resources/Test_L2_R1_Middle_Easy.txt");
-
-        try {
-            Scanner scanner = new Scanner(file);
-
-            while (scanner.hasNextLine()) {
-                count++;
-                String line = scanner.nextLine();
-                String[] parts = line.split(" ");
-                String moveOrder = parts[0];
-                int score = Integer.parseInt(parts[1]);
-
-                for (int i = 0; i < moveOrder.length(); i++) {
-                    board.placeDisc(Character.getNumericValue(moveOrder.charAt(i) - 1), player);
-                    player = solver.getOpponent(player);
-                }
-
-                int testScore = solver.solve(player);
-
-                if (testScore == score) {
-                    System.out.println("True " + count);
-                } else {
-                    System.out.println("False: " + testScore + " != " + score + " " + count);
-                }
-                
-                assertEquals(score, testScore, "dasdad");
-
-                board.clear();
-
-            }
-        } catch (FileNotFoundException fileNotFoundException) {
-            System.out.printf("Test_L3_R1_End_Easy.txt not found");
+    private static List<String> loadDataFromFile(Path filePath) throws FileNotFoundException {
+        try (Scanner scanner = new Scanner(filePath.toFile())) {
+            //return scanner.tokens().collect(Collectors.toList());
+            return scanner.useDelimiter("\n")
+                          .tokens()
+                          .collect(Collectors.toList());
         }
     }
 
-
-    //@Test
-    void MiddleMediunTest() {
-
-        System.out.println("Test_L2_R2_Middle_Medium.txt");
-
-        File file = new File("src/test/resources/Test_L2_R2_Middle_Medium.txt");
-
-        try {
-            Scanner scanner = new Scanner(file);
-
-            while (scanner.hasNextLine()) {
-                count++;
-                String line = scanner.nextLine();
-                String[] parts = line.split(" ");
-                String moveOrder = parts[0];
-                int score = Integer.parseInt(parts[1]);
-
-                for (int i = 0; i < moveOrder.length(); i++) {
-                    board.placeDisc(Character.getNumericValue(moveOrder.charAt(i) - 1), player);
-                    player = solver.getOpponent(player);
-                }
-
-                int testScore = solver.solve(player);
-
-                if (testScore == score) {
-                    System.out.println("True " + count);
-                } else {
-                    System.out.println("False: " + testScore + " != " + score + " " + count);
-                }
-                
-                assertEquals(score, testScore, "dasdad");
-
-                board.clear();
-
-            }
-        } catch (FileNotFoundException fileNotFoundException) {
-            System.out.printf("Test_L3_R1_End_Easy.txt not found");
-        }
+    private static Stream<String> testFilesProvider() {
+        return Stream.of(
+            "src/test/resources/Test_L3_R1_End_Easy.txt",
+            "src/test/resources/Test_L2_R1_Middle_Easy.txt",
+            "src/test/resources/Test_L2_R2_Middle_Medium.txt",
+            "src/test/resources/Test_L1_R1_Begin_Easy.txt",
+            "src/test/resources/Test_L1_R2_Begin_Medium.txt",
+            "src/test/resources/Test_L1_R3_Begin_Hard.txt"
+        );
     }
 
-    @Test
-    void BeginEasyTest() {
-
-        System.out.println("Test_L1_R3_Begin_Easy.txt");
-
-        File file = new File("src/test/resources/Test_L1_R3_Begin_Easy.txt");
-
-        try {
-            Scanner scanner = new Scanner(file);
-
-            while (scanner.hasNextLine()) {
-                count++;
-                String line = scanner.nextLine();
-                String[] parts = line.split(" ");
-                String moveOrder = parts[0];
-                int score = Integer.parseInt(parts[1]);
-
-                for (int i = 0; i < moveOrder.length(); i++) {
-                    board.placeDisc(Character.getNumericValue(moveOrder.charAt(i) - 1), player);
-                    player = solver.getOpponent(player);
-                }
-
-                int testScore = solver.solve(player);
-
-                if (testScore == score) {
-                    System.out.println("True " + count);
-                } else {
-                    System.out.println("False: " + testScore + " != " + score + " " + count);
-                }
-                
-                assertEquals(score, testScore, "dasdad");
-
-                board.clear();
-
-            }
-        } catch (FileNotFoundException fileNotFoundException) {
-            System.out.printf("Test_L3_R1_End_Easy.txt not found");
-        }
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         long start = System.nanoTime(); 
-        testNegamax("backend/src/test/resources/Test_L1_R1_Begin_Easy.txt");
+        int count = testNegamax("src/test/resources/Test_L2_R2_Middle_Medium.txt");
         long end = System.nanoTime();
         System.out.println("time: " + ((end - start) / 1000000.0) + "ms");
         System.out.println("time: " + ((end - start) / 1000000.0 / count) + "ms/position");
         System.out.println(count);
     }
 
-    public static void testNegamax(String filename) {
+    public static int testNegamax(String filename) {
+        int count = 0;
         File file = new File(filename);
         BitBoard board = new BitBoard();
         Solver solver = new Solver(board);
@@ -222,7 +98,7 @@ public class SolverTest {
                 }
                 
 
-                int testScore = solver.solve(player);
+                int testScore = solver.solve(board, player);
                 if (testScore == score) {
                     System.out.println("True " + count);
                 } else {
@@ -232,13 +108,13 @@ public class SolverTest {
                 board.clear();
                 //break;
                 if (count >= 1000) {
-                    return;
+                    return count;
                 }
             }
         } catch (FileNotFoundException fileNotFoundException) {
             System.out.printf("%s not found", filename);
         }
-        
+        return count;
 
     }
 
