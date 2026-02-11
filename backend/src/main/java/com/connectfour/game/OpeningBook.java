7aefe6b26f2cc4fa745490a10e38f3272fb97869
Brayden T <mailforbrade@gmail.com>
2026-02-11 00:33:10 -0500

    Improve loading speed of OpeningBook

--- backend/src/main/java/com/connectfour/game/OpeningBook.java
+++ backend/src/main/java/com/connectfour/game/OpeningBook.java
@@ -1,57 +1,72 @@
 package com.connectfour.game;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.file.Paths;
 import it.unimi.dsi.fastutil.longs.Long2ByteOpenHashMap;
+
+
 
 public class OpeningBook {
 
-    private String filepath;
-    Long2ByteOpenHashMap map;
+    public enum BookType {
+        EIGHT_MOVES("src/main/resources/opening_book_8_moves.bin", 8, 196686),
+        SIX_MOVES("src/main/resources/opening_book_6_moves.bin", 6, 19445),
+        EIGHT_MOVES_DEBUG("backend/src/main/resources/opening_book_8_moves.bin", 8, 196686),
+        SIX_MOVES_DEBUG("backend/src/main/resources/opening_book_6_moves.bin", 6, 19445);
 
-    public OpeningBook(String filepath) {
-        this.filepath = Paths.get(filepath).toString();
-        this.map = new Long2ByteOpenHashMap();
+        private final String path;
+        private final int moveLength;
+        private final int entries;
+
+        BookType(String path, int moveLength, int entries) {
+            this.path = path;
+            this.moveLength = moveLength;
+            this.entries = entries;
+        }
+
+        public String getPath() {
+            return path;
+        }
+
+        public int getMoveLength() {
+            return moveLength;
+        }
+
+        public int getEntries() {
+            return entries;
+        }
+    }
+
+    private String filepath;
+    private int entries;
+    private final int BUFFER_SIZE = 1 << 16;
+    private Long2ByteOpenHashMap map;
+
+    public OpeningBook(BookType book) {
+        this.filepath = book.getPath();
+        this.entries = book.getEntries();
+        this.map = new Long2ByteOpenHashMap(book.getEntries(), 0.75f);
+        map.defaultReturnValue(Byte.MIN_VALUE);
+        load();
     }
 
     public void load() {
+        try (DataInputStream in = new DataInputStream(
+            new BufferedInputStream(new FileInputStream(filepath), BUFFER_SIZE))) {
 
-        BitBoard board = new BitBoard();
-
-        try (FileInputStream fis = new FileInputStream(filepath)) {
-
-            while (fis.available() > 0) {
-
-                byte[] keyBytes = new byte[4];
-                if (fis.read(keyBytes) != 4) {
-                    break; 
-                }
-
-                ByteBuffer keyBuffer = ByteBuffer.wrap(keyBytes);
-                keyBuffer.order(ByteOrder.BIG_ENDIAN);
-                int key = keyBuffer.getInt();
-
-                byte[] valueBytes = new byte[2];
-                if (fis.read(valueBytes) != 2) {
-                    break;
-                }
-                ByteBuffer valueBuffer = ByteBuffer.wrap(valueBytes);
-                valueBuffer.order(ByteOrder.BIG_ENDIAN);
-                short value = valueBuffer.getShort();
-
-                board.load(String.valueOf(key));
-                
-                map.put(board.key(), (byte) value);
-
-                board.clear();
+            for (int i = 0; i < entries; i++) {
+                long key = in.readLong();
+                byte value = in.readByte();
+                map.put(key, value);
             }
-            
+        }
+        catch (FileNotFoundException e) {
+
         } catch (IOException e) {
-            //e.printStackTrace();
-            System.out.printf("Error: Cannot find %s\n", filepath);
+            
         }
     }
 
