2e3aec378f621412e59bab23c85b482642c53878
Brayden T <mailforbrade@gmail.com>
2026-02-17 02:21:01 -0500

    Add documentation to Player enum

--- backend/src/main/java/com/connectfour/game/Player.java
+++ backend/src/main/java/com/connectfour/game/Player.java
@@ -1,9 +1,23 @@
 package com.connectfour.game;
 
+/**
+ * Enum to represent the different players in a Connect Four game,
+ * which are Red and Yellow
+ * 
+ * @author Brayden T
+ */
+
 public enum Player {
+    // Different players
     RED,
     YELLOW;
 
+    /**
+     * Gets the opponent of the given player
+     * 
+     * @param player The player to find the opponent of
+     * @return The opposite player
+     */
     public static Player opponent(Player player) {
         return (player == Player.RED) ? Player.YELLOW : Player.RED;
     }
